45bda09934f8f7b9929152170d1484d336921458
Nahiyan Ishtiaque <nahiyan.ishtiaque@gmail.com>
2024-09-23 14:31:26 -0400

    delete redundancies

--- training.py
+++ training.py
@@ -12,7 +12,6 @@ nltk.download('wordnet')
 
 lemmatizer = WordNetLemmatizer()
 
-# Load intents
 with open('intents.json') as file:
     intents = json.load(file)
 
@@ -21,10 +20,9 @@ classes = []
 documents = []
 ignoreLetters = ['?', '!', '.', ',']
 
-# Tokenize patterns and create documents
 for intent in intents['intents']:
     for pattern in intent['patterns']:
-        wordList = nltk.word_tokenize(pattern)  # Tokenize each pattern
+        wordList = nltk.word_tokenize(pattern)
         words.extend(wordList)
         documents.append((wordList, intent['tag']))
         if intent['tag'] not in classes:
@@ -34,7 +32,6 @@ words = [lemmatizer.lemmatize(word.lower()) for word in words if word not in ign
 words = sorted(set(words))
 classes = sorted(set(classes))
 
-# Corrected this line to dump 'classes' instead of 'words' twice
 pickle.dump(words, open('words.pkl', 'wb'))
 pickle.dump(classes, open('classes.pkl', 'wb'))
 
@@ -44,7 +41,6 @@ output_empty = [0] * len(classes)
 for document in documents:
     bag = []
     wordPatterns = document[0]
-    # Fix missing parentheses here
     wordPatterns = [lemmatizer.lemmatize(word.lower()) for word in wordPatterns]
     for word in words:
         bag.append(1) if word in wordPatterns else bag.append(0)
@@ -55,11 +51,9 @@ for document in documents:
 
 random.shuffle(training)
 
-# Separate the features (trainX) and labels (trainY)
-trainX = np.array([t[0] for t in training])  # Extract the bags of words
-trainY = np.array([t[1] for t in training])  # Extract the output labels
+trainX = np.array([t[0] for t in training])  
+trainY = np.array([t[1] for t in training])  
 
-# Building the neural network model
 model = tf.keras.Sequential()
 model.add(tf.keras.layers.Dense(128, input_shape=(len(trainX[0]),), activation='relu'))
 model.add(tf.keras.layers.Dropout(0.5))
@@ -67,13 +61,10 @@ model.add(tf.keras.layers.Dense(64, activation='relu'))
 model.add(tf.keras.layers.Dropout(0.5))
 model.add(tf.keras.layers.Dense(len(trainY[0]), activation='softmax'))
 
-# Using Stochastic Gradient Descent as the optimizer
 sgd = tf.keras.optimizers.SGD(learning_rate=0.01, momentum=0.9, nesterov=True)
 model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
 
-# Train the model
 hist = model.fit(trainX, trainY, epochs=200, batch_size=5, verbose=1)
 
-# Save the trained model
 model.save('chatbot_model.h5', hist)
 print('Done')
