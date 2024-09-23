04fc0d3c4c88c4c900269099125e0183e74889d3
Nahiyan Ishtiaque <nahiyan.ishtiaque@gmail.com>
2024-09-23 14:29:20 -0400

    created chatbot interface + added trained model

--- training.py
+++ training.py
@@ -7,13 +7,14 @@ import tensorflow as tf
 import nltk
 from nltk.stem import WordNetLemmatizer
 
-nltk.download('punkt_tab')
+nltk.download('punkt')
 nltk.download('wordnet')
 
 lemmatizer = WordNetLemmatizer()
 
 # Load intents
-intents = json.loads(open('intents.json').read())
+with open('intents.json') as file:
+    intents = json.load(file)
 
 words = []
 classes = []
@@ -29,12 +30,13 @@ for intent in intents['intents']:
         if intent['tag'] not in classes:
             classes.append(intent['tag'])
 
-words = [lemmatizer.lemmatize(word) for word in words if word not in ignoreLetters]
+words = [lemmatizer.lemmatize(word.lower()) for word in words if word not in ignoreLetters]
 words = sorted(set(words))
 classes = sorted(set(classes))
 
+# Corrected this line to dump 'classes' instead of 'words' twice
 pickle.dump(words, open('words.pkl', 'wb'))
-pickle.dump(words, open('classes.pkl', 'wb'))
+pickle.dump(classes, open('classes.pkl', 'wb'))
 
 training = []
 output_empty = [0] * len(classes)
@@ -42,7 +44,8 @@ output_empty = [0] * len(classes)
 for document in documents:
     bag = []
     wordPatterns = document[0]
-    wordPatterns = [lemmatizer.lemmatize(word.lower() for word in wordPatterns)]
+    # Fix missing parentheses here
+    wordPatterns = [lemmatizer.lemmatize(word.lower()) for word in wordPatterns]
     for word in words:
         bag.append(1) if word in wordPatterns else bag.append(0)
     
@@ -51,7 +54,26 @@ for document in documents:
     training.append([bag, output_row])
 
 random.shuffle(training)
-training = np.array(training)
 
-trainX = list(training[:, 0])
-trainY = list(training[:, 1])
+# Separate the features (trainX) and labels (trainY)
+trainX = np.array([t[0] for t in training])  # Extract the bags of words
+trainY = np.array([t[1] for t in training])  # Extract the output labels
+
+# Building the neural network model
+model = tf.keras.Sequential()
+model.add(tf.keras.layers.Dense(128, input_shape=(len(trainX[0]),), activation='relu'))
+model.add(tf.keras.layers.Dropout(0.5))
+model.add(tf.keras.layers.Dense(64, activation='relu'))
+model.add(tf.keras.layers.Dropout(0.5))
+model.add(tf.keras.layers.Dense(len(trainY[0]), activation='softmax'))
+
+# Using Stochastic Gradient Descent as the optimizer
+sgd = tf.keras.optimizers.SGD(learning_rate=0.01, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+
+# Train the model
+hist = model.fit(trainX, trainY, epochs=200, batch_size=5, verbose=1)
+
+# Save the trained model
+model.save('chatbot_model.h5', hist)
+print('Done')
